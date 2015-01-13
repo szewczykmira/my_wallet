45242385be7f25ee0e8eb4b46fe3d5159004fba0
szewczykmira <szewczyk.mira@gmail.com>
2015-01-13 19:22:26 +0100

    User model added with validation

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable
+  # validation
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :email, presence: true
+
+end
