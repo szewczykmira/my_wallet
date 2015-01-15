7e491a3eaf24af5539d8ebc57a3e6dea03f4ab75
szewczykmira <szewczyk.mira@gmail.com>
2015-01-15 18:19:19 +0100

    Some changes for devise in user model added - mostly redirecting and other stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
+  attr_accessor :username
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable, 
          :recoverable, :rememberable, :trackable, :validatable
   # validation
   validates :username, presence: true
