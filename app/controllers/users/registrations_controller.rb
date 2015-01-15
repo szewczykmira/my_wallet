6e0f31158057a7c38fca9e2d07494e10ca55e612
szewczykmira <szewczyk.mira@gmail.com>
2015-01-15 18:29:39 +0100

    Default redirecting is back

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -49,9 +49,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  def after_sign_up_path_for(resource)
-    after_sign_in_path_for(resource)
-  end
+  # def after_sign_up_path_for(resource)
+  #  super
+  # end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
