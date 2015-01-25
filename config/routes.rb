73d7f1eaf2a0bcc70e837361f37ea290a8045b4d
szewczykmira <szewczyk.mira@gmail.com>
2015-01-25 22:35:27 +0100

    Custom form for loans added, route for loans#create added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'my_wallet/index'
+  resources :loans, :only => [:create]
 
   devise_for :users, :controllers => { registrations: 'users/registrations'}
   # The priority is based upon order of creation: first created -> highest priority.
