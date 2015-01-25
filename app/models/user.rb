d734c3f146bdfc661a6887a5f36187de123fe755
szewczykmira <szewczyk.mira@gmail.com>
2015-01-25 22:43:54 +0100

    Fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true
 
   has_many :expenses
-  has_many :debts, :class_name => 'Expense', :foreign_key => :debtor_id
-  has_many :credits, :class_name => 'Expense', :foreign_key => :creditor_id
+  has_many :debts, :class_name => 'Loan', :foreign_key => :debtor_id
+  has_many :credits, :class_name => 'Loan', :foreign_key => :creditor_id
 
 end
