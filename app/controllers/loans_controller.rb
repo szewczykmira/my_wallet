3535d894719954e7585d0361216b48bac41b5702
szewczykmira <szewczyk.mira@gmail.com>
2015-07-14 19:20:34 +0200

    Handling errors in ajax form for creating credits added

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -8,11 +8,11 @@ class LoansController < ApplicationController
     @loan.accepted = true
     respond_to do |format|
       if @loan.save
+        @saved = true
         format.js
       else
-        format.html { render 'my_wallet/index' }
-        @expense = Expense.new
-        format.json { render json: @loan.errors, status: :unprocessable_entity }
+        @saved = false
+        format.js
       end
     end
   end
